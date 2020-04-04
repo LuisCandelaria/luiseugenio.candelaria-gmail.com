f15a1cd3d80bb7421833a9d0c939c4ff15f91d3c
Luis Eugenio Candelaria Azpilcueta <luiseugenio.candelaria@gmail.com>
2020-04-04 15:52:27 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1>
+    Laboratory 6 - “YouTube APP” 4-apr-20
+</h1>
+<h3>
+    Luis Eugenio Candelaria Azpilcueta A00816826
+</h3>
+<br>
+<h4>
+    Description:
+</h4>
+<p>
+For this laboratory you will have to build a page with a simple search form that allows the user to search YouTube videos.
+</p>
---
+++ js/index.js
@@ -0,0 +1,111 @@
+const API_KEY = "AIzaSyDhoQPvet_kqLp6wC2GM_f0MnaFFEEA8IA";
+var page = 10;
+var data2;
+
+function fetchVideos( searchTerm ){
+    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&type=video&key=${API_KEY}&order=relevance&maxResults=10`;
+    let settings = {
+        method : 'GET'
+    };
+    fetch(url, settings)
+        .then( response => {
+            if(response.ok){
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then( responseJSON => {
+            displayResults(responseJSON);
+        })
+        .catch( err => {
+            console.log(err);
+        });
+}
+
+function displayResults( data ){
+    console.log(data);
+    len = data.items.length;
+    let results = document.querySelector('.results');
+    results.innerHTML = "";
+    for(let i = 0; i < page; i++){
+        results.innerHTML += `
+            <div class="video">
+                <a class="link" href="https://www.youtube.com/watch?v=${data.items[i].id.videoId}" target="_blank">${data.items[i].snippet.title}</a>
+                <br>
+                <a href="https://www.youtube.com/watch?v=${data.items[i].id.videoId}" target="_blank">
+                    <img src="${data.items[i].snippet.thumbnails.medium.url}" />
+                </a>
+            </div>
+        `;
+    }
+    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&type=video&key=${API_KEY}&maxResults=10&order=relevance&pageToken=${data.nextPageToken}`;
+    let settings = {
+        method : 'GET'
+    };
+    fetch(url, settings)
+        .then( response => {
+            if(response.ok){
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then ( responseJSON => {
+            data2 = responseJSON;
+        })
+        .catch( err => {
+            console.log(err);
+        });
+    results.innerHTML += `
+        <button class="nextBtn" type="button" onclick="displayNxtPage()">
+            <span>
+                Next page
+            </span>
+        </button>
+    `;
+}
+
+function displayNxtPage(){
+    console.log(data2);
+    let results = document.querySelector('.results');
+    for(let i = 0; i < page; i++){
+        results.innerHTML += `
+            <div class="video">
+                <a class="link" href="https://www.youtube.com/watch?v=${data2.items[i].id.videoId}" target="_blank">${data2.items[i].snippet.title}</a>
+                <br>
+                <a href="https://www.youtube.com/watch?v=${data2.items[i].id.videoId}" target="_blank">
+                    <img src="${data2.items[i].snippet.thumbnails.medium.url}" />
+                </a>
+            </div>
+        `;
+    }
+    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&type=video&key=${API_KEY}&maxResults=10&order=relevance&pageToken=${data2.nextPageToken}`;
+    let settings = {
+        method : 'GET'
+    };
+    fetch(url, settings)
+        .then( response => {
+            if(response.ok){
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then ( responseJSON => {
+            data2 = responseJSON;
+        })
+        .catch( err => {
+            console.log(err);
+        });
+    results.innerHTML += `
+    <button class="nextBtn" type="button" onclick="displayNxtPage()">
+        <span>
+            Next page
+        </span>
+    </button>
+    `;
+}
+
+function watchForm(){
+    event.preventDefault();
+    let searchTerm = document.querySelector('#searchTerm').value;
+    fetchVideos(searchTerm);
+}
